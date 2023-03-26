a218e97a06c916a01e9f77568c6c2f86c856ac57
Peter Calvanelli <peter.j.calvanelli@gmail.com>
2023-03-25 21:07:37 -0400

    bleh

--- pages/signup.js
+++ pages/signup.js
@@ -1,131 +1,87 @@
 // SignUp.js
-
-import React, { useState } from 'react';
-import styles from 'styles/SignUp.module.css';
+import { useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
-import { useRouter } from 'next/router';
 import NavBar from '@/components/NavBar';
 
 const supabaseUrl = 'https://zgjfvxglyiydrytbhuwc.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpnamZ2eGdseWl5ZHJ5dGJodXdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzg0NzcyMTAsImV4cCI6MTk5NDA1MzIxMH0.WGZHEZGAA8YQ2oJikbmanrhJtScfZCNdUNQh0DzdRKU';
-const supabase = createClient(supabaseUrl, supabaseKey);
 
-function SignUp() {
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-  });
 
-  const [formErrors, setFormErrors] = useState({
-    email: '',
-    password: '',
-  });
+export const supabase = createClient(supabaseUrl, supabaseKey);
 
-  const [submitted, setSubmitted] = useState(false);
-  const [successMessage, setSuccessMessage] = useState('');
 
-  const router = useRouter();
 
-const handleSubmit = async (e) => {
-  e.preventDefault();
+const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState(null);
 
-  const { email, password } = formData;
+  const handleSignUp = async (event) => {
+    event.preventDefault();
 
-  try {
-      const { user } = await supabase.auth.signUp({
-        email,
-        password,
-      });
-
-      if (user) {
-        setSuccessMessage('Thanks for signing up!');
-        setFormData({
-          email: '',
-          password: '',
-        });
-        setSubmitted(true);
-        router.push('/success'); // Redirect to the Success page
-      }
-    } catch (error) {
-      console.log(error);
-
-      if (error.code === '23505') {
-        setFormErrors({
-          email: 'This email is already registered.',
-          password: '',
-        });
-      } else {
-        setFormErrors({
-          email: '',
-          password: 'Something went wrong. Please try again later.',
-        });
-      }
+    // Form validation
+    if (!email || !password || !confirmPassword) {
+      setError('Please fill in all fields');
+      return;
     }
-};
 
+    if (password !== confirmPassword) {
+      setError('Passwords do not match');
+      return;
+    }
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
+    const { error: signUpError } = await supabase.auth.signUp({
+      email,
+      password,
+    });
 
-    setFormData((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
+    if (signUpError) {
+      setError(signUpError.message);
+    }
+  };
 
-    setFormErrors((prevState) => ({
-      ...prevState,
-      [name]: '',
-    }));
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPassword(event.target.value);
   };
 
   return (
-    <div>
-    < NavBar />
-    <div className={styles.container}>
-      <div className={styles.card}>
-        <img className ={styles.logo} src='/hippo_logo-removebg-preview.png' alt='Off Chain Data' />
-        <h1 className={styles.title}>Create your account</h1>
-        {submitted && successMessage && (
-          <p className={styles.successMessage}>{successMessage}</p>
-        )}
-        <form className={styles.signUpForm} onSubmit={handleSubmit}>
-          <div className={styles.formGroup}>
-            <input
-              placeholder='Email Address'
-              className={`${styles.formField} ${formErrors.email && styles.formFieldError}`}
-              type='email'
-              id='email'
-              name='email'
-              value={formData.email}
-              onChange={handleChange}
-            />
-            {formErrors.email && (
-              <p className={styles.errorMessage}>{formErrors.email}</p>
-            )}
-
-          </div>
-          <div className={styles.formGroup}>
-            <input
-              placeholder='Password'
-              className={`${styles.formField} ${formErrors.password && styles.formFieldError}`}
-              type='password'
-              id='password'
-                name='password'
-                value={formData.password}
-                onChange={handleChange}
-            />
-            {formErrors.password && (
-                <p className={styles.errorMessage}>{formErrors.password}</p>
-            )}
-            </div>
-            <button className={styles.submitButton} type='submit'>
-                Sign Up
-            </button>
-        </form>
-        </div>
-    </div>
-    </div>
-    );
-}
+    <form onSubmit={handleSignUp}>
+      {error && <div>{error}</div>}
+      <label>
+        Email:
+        <input type="email" value={email} onChange={handleEmailChange} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+      </label>
+      <br />
+      <label>
+        Confirm Password:
+        <input
+          type="password"
+          value={confirmPassword}
+          onChange={handleConfirmPasswordChange}
+        />
+      </label>
+      <br />
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+};
 
 export default SignUp;
