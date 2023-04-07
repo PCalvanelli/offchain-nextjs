361f90e568b27c9d20c6940085c871a1f08067a5
Dhairya Rathod <44311424+dhairya-rathod@users.noreply.github.com>
2023-04-07 13:04:43 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -8,17 +8,6 @@ import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
 import ErrorBoundary from '@/components/ErrorBoundary';
 
-// Check that PostHog is client-side (used to handle Next.js SSR)
-if (typeof window !== 'undefined') {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
-    // Disable in development
-    loaded: (posthog) => {
-      if (process.env.NODE_ENV === 'development') posthog.opt_out_capturing()
-    }
-  })
-}
-
 export default function App({ Component, pageProps }) {
   const router = useRouter()
 
